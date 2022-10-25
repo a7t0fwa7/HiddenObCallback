11f018df8e7f09eca2ff3f37e732f3ed5b91e5a2
rogxo <1910500309@qq.com>
2022-10-25 18:17:28 +0800

    鸡腿哥提供的思路

---
+++ HiddenObCallback/Utils.h
@@ -0,0 +1,121 @@
+#include <ntifs.h>
+#include <windef.h>
+#include <ntimage.h>
+
+NTKERNELAPI UCHAR* PsGetProcessImageFileName(__in PEPROCESS Process);
+
+PVOID GetDriverEntryByImageBase(PVOID ImageBase)
+{
+	PIMAGE_DOS_HEADER pDOSHeader;
+	PIMAGE_NT_HEADERS64 pNTHeader;
+	PVOID pEntryPoint;
+	pDOSHeader = (PIMAGE_DOS_HEADER)ImageBase;
+	pNTHeader = (PIMAGE_NT_HEADERS64)((ULONG64)ImageBase + pDOSHeader->e_lfanew);
+	pEntryPoint = (PVOID)((ULONG64)ImageBase + pNTHeader->OptionalHeader.AddressOfEntryPoint);
+	return pEntryPoint;
+}
+
+
+VOID UnicodeToChar(PUNICODE_STRING dst, char* src)
+{
+	ANSI_STRING string;
+	if (!src || !dst || !dst->Buffer) return;
+	if (RtlUnicodeStringToAnsiString(&string, dst, TRUE) == STATUS_SUCCESS) {
+		strcpy(src, string.Buffer);
+		RtlFreeAnsiString(&string);
+	}
+}
+
+KIRQL WP_Off()
+{
+	KIRQL irql = KeRaiseIrqlToDpcLevel();
+	ULONG_PTR cr0 = __readcr0();
+
+	cr0 &= 0xfffffffffffeffff;
+	_disable();
+	__writecr0(cr0);
+
+	return irql;
+}
+
+VOID WP_On(KIRQL irql)
+{
+
+	ULONG_PTR cr0 = __readcr0();
+	cr0 |= 0x10000;
+	__writecr0(cr0);
+	_enable();
+
+	KeLowerIrql(irql);
+}
+
+
+//0x120 bytes (sizeof)
+typedef struct _LDR_DATA_TABLE_ENTRY
+{
+    struct _LIST_ENTRY InLoadOrderLinks;                                    //0x0
+    struct _LIST_ENTRY InMemoryOrderLinks;                                  //0x10
+    struct _LIST_ENTRY InInitializationOrderLinks;                          //0x20
+    VOID* DllBase;                                                          //0x30
+    VOID* EntryPoint;                                                       //0x38
+    ULONG SizeOfImage;                                                      //0x40
+    struct _UNICODE_STRING FullDllName;                                     //0x48
+    struct _UNICODE_STRING BaseDllName;                                     //0x58
+    union
+    {
+        UCHAR FlagGroup[4];                                                 //0x68
+        ULONG Flags;                                                        //0x68
+        struct
+        {
+            ULONG PackagedBinary : 1;                                         //0x68
+            ULONG MarkedForRemoval : 1;                                       //0x68
+            ULONG ImageDll : 1;                                               //0x68
+            ULONG LoadNotificationsSent : 1;                                  //0x68
+            ULONG TelemetryEntryProcessed : 1;                                //0x68
+            ULONG ProcessStaticImport : 1;                                    //0x68
+            ULONG InLegacyLists : 1;                                          //0x68
+            ULONG InIndexes : 1;                                              //0x68
+            ULONG ShimDll : 1;                                                //0x68
+            ULONG InExceptionTable : 1;                                       //0x68
+            ULONG ReservedFlags1 : 2;                                         //0x68
+            ULONG LoadInProgress : 1;                                         //0x68
+            ULONG LoadConfigProcessed : 1;                                    //0x68
+            ULONG EntryProcessed : 1;                                         //0x68
+            ULONG ProtectDelayLoad : 1;                                       //0x68
+            ULONG ReservedFlags3 : 2;                                         //0x68
+            ULONG DontCallForThreads : 1;                                     //0x68
+            ULONG ProcessAttachCalled : 1;                                    //0x68
+            ULONG ProcessAttachFailed : 1;                                    //0x68
+            ULONG CorDeferredValidate : 1;                                    //0x68
+            ULONG CorImage : 1;                                               //0x68
+            ULONG DontRelocate : 1;                                           //0x68
+            ULONG CorILOnly : 1;                                              //0x68
+            ULONG ChpeImage : 1;                                              //0x68
+            ULONG ReservedFlags5 : 2;                                         //0x68
+            ULONG Redirected : 1;                                             //0x68
+            ULONG ReservedFlags6 : 2;                                         //0x68
+            ULONG CompatDatabaseProcessed : 1;                                //0x68
+        };
+    };
+    USHORT ObsoleteLoadCount;                                               //0x6c
+    USHORT TlsIndex;                                                        //0x6e
+    struct _LIST_ENTRY HashLinks;                                           //0x70
+    ULONG TimeDateStamp;                                                    //0x80
+    struct _ACTIVATION_CONTEXT* EntryPointActivationContext;                //0x88
+    VOID* Lock;                                                             //0x90
+    struct _LDR_DDAG_NODE* DdagNode;                                        //0x98
+    struct _LIST_ENTRY NodeModuleLink;                                      //0xa0
+    struct _LDRP_LOAD_CONTEXT* LoadContext;                                 //0xb0
+    VOID* ParentDllBase;                                                    //0xb8
+    VOID* SwitchBackContext;                                                //0xc0
+    struct _RTL_BALANCED_NODE BaseAddressIndexNode;                         //0xc8
+    struct _RTL_BALANCED_NODE MappingInfoIndexNode;                         //0xe0
+    ULONGLONG OriginalBase;                                                 //0xf8
+    union _LARGE_INTEGER LoadTime;                                          //0x100
+    ULONG BaseNameHashValue;                                                //0x108
+    enum _LDR_DLL_LOAD_REASON LoadReason;                                   //0x10c
+    ULONG ImplicitPathOptions;                                              //0x110
+    ULONG ReferenceCount;                                                   //0x114
+    ULONG DependentLoadFlags;                                               //0x118
+    UCHAR SigningLevel;                                                     //0x11c
+}LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
---
+++ HiddenObCallback/entry.c
@@ -0,0 +1,202 @@
+#include <ntifs.h>
+#include "Utils.h"
+
+NTSYSAPI NTSTATUS NTAPI ObReferenceObjectByName(
+	__in PUNICODE_STRING ObjectName,
+	__in ULONG Attributes,
+	__in_opt PACCESS_STATE AccessState,
+	__in_opt ACCESS_MASK DesiredAccess,
+	__in POBJECT_TYPE ObjectType,
+	__in KPROCESSOR_MODE AccessMode,
+	__inout_opt PVOID ParseContext,
+	__out PVOID* Object
+);
+
+extern POBJECT_TYPE* IoDriverObjectType;
+
+
+HANDLE g_ObCallbackHandle = NULL;	//对象回调的标识
+
+PVOID GetRelayAddress()
+{
+	UNICODE_STRING FunName = { 0 };
+	RtlInitUnicodeString(&FunName, L"NtQueryInformationProcess");
+
+	/*
+	fffff801`772642c0 488d1539bd99ff  lea     rdx,[nt!VrpRegistryString <PERF> (nt+0x0) (fffff801`76c00000)]
+	fffff801`772642c7 8b8c82a85c6600  mov     ecx,dword ptr [rdx+rax*4+665CA8h]
+	fffff801`772642ce 4803ca          add     rcx,rdx
+	fffff801`772642d1 e8eafd3a00      call    nt!_guard_retpoline_switchtable_jump_rcx (fffff801`776140c0)
+	fffff801`772642d6 cc              int     3
+	*/
+	//DbgBreakPoint();
+
+	PUCHAR pfnNtQueryInformationProcess = (PUCHAR)MmGetSystemRoutineAddress(&FunName);
+
+	if (pfnNtQueryInformationProcess && MmIsAddressValid(pfnNtQueryInformationProcess) && MmIsAddressValid((PVOID)((ULONG64)pfnNtQueryInformationProcess + 0x3D48)))
+	{
+		for (PUCHAR start = pfnNtQueryInformationProcess; start < pfnNtQueryInformationProcess + 0x3D48; ++start)
+		{
+			if (*(PULONG)start == 0xE8CA0348 && start[0x8] == 0xCC)
+			{
+				start += 3;
+				//return start + *(PLONG)(start + 1) + 5;
+				return start;
+			}
+		}
+	}
+	return 0;
+}
+
+
+BYTE buffer[6] = { 0 };
+BOOLEAN ChangeMmVerifyCallback(BOOLEAN IsNop)
+{
+	KIRQL irql = { 0 };
+	UNICODE_STRING FunName = { 0 };
+	RtlInitUnicodeString(&FunName, L"ObRegisterCallbacks");
+
+	/*
+	PAGE:00000001407B4CAB 0F 84 09 57 0A 00                 jz      loc_14085A3BA
+	PAGE:00000001407B4CB1 BA 20 00 00 00                    mov     edx, 20h
+	PAGE:00000001407B4CB6 E8 15 AA BF FF                    call    MmVerifyCallbackFunctionCheckFlags
+	PAGE:00000001407B4CBB 85 C0                             test    eax, eax
+	PAGE:00000001407B4CBD 0F 84 21 57 0A 00                 jz      loc_14085A3E4
+	PAGE:00000001407B4CC3 49 8B 4E 18                       mov     rcx, [r14+18h]
+	*/
+	//DbgBreakPoint();
+
+	PUCHAR pfnObRegisterCallbacks = (PUCHAR)MmGetSystemRoutineAddress(&FunName);
+
+	if (pfnObRegisterCallbacks && MmIsAddressValid(pfnObRegisterCallbacks) && MmIsAddressValid((PVOID)((ULONG64)pfnObRegisterCallbacks + 0x1CA)))
+	{
+		for (PUCHAR start = pfnObRegisterCallbacks; start < pfnObRegisterCallbacks + 0x1CA; ++start)
+		{
+			if (*(PULONG)start == 0x000020BA && start[0x5] == 0xE8 && *(PUINT16)(start + 0xA) == 0xC085)
+			{
+				start += 0xC;
+				irql = WP_Off();
+				if (IsNop)
+				{
+					if (buffer[0] == 0)
+					{
+						memcpy(buffer, start, 6);
+						memset(start, 0x90, 6);
+					}
+				}
+				else
+				{
+					//*(PULONG)start = 0x5721840F;
+					//*(PUINT16)start = 0x000A;
+					if (buffer[0] != 0)
+					{
+						memcpy(start, buffer, 6);
+						memset(buffer, 0x00, 6);
+					}
+				}
+				WP_On(irql);
+			}
+		}
+	}
+	return 0;
+}
+
+
+OB_PREOP_CALLBACK_STATUS ObjectPreCallback(
+	_In_  PVOID RegistrationContext,
+	_In_  POB_PRE_OPERATION_INFORMATION OperationInformation
+)
+{
+	NTSTATUS ntstatus = STATUS_SUCCESS;
+	PUCHAR pszName = NULL;
+	HANDLE pid = NULL;
+
+	if (OperationInformation->ObjectType == *PsProcessType && OperationInformation->Object != NULL)
+	{
+		pszName = PsGetProcessImageFileName(OperationInformation->Object);
+		pid = PsGetProcessId(OperationInformation->Object);
+
+		if (pszName != NULL)
+		{
+			DbgPrint("[+] ProcessName:%s --- ProcessID:%lld --- \n", pszName, (DWORD64)pid);
+		}
+	}
+
+	ntstatus = OB_PREOP_SUCCESS;
+	return ntstatus;
+}
+
+
+NTSTATUS RegisterHiddenObCallback()
+{
+	NTSTATUS ntstatus = STATUS_SUCCESS;
+	OB_CALLBACK_REGISTRATION callback = { 0 };
+	OB_OPERATION_REGISTRATION operation = { 0 };
+	PVOID Relay = GetRelayAddress();
+	if (Relay == NULL)
+	{
+		KdPrint(("GetRelayAddress Failed\n"));
+		return STATUS_UNSUCCESSFUL;
+	}
+	KdPrint(("Relay = %p\n",Relay));
+
+	operation.ObjectType = PsProcessType;
+	operation.Operations = OB_OPERATION_HANDLE_CREATE | OB_OPERATION_HANDLE_DUPLICATE;	//3
+	//operation.PreOperation = ObjectPreCallback;
+	operation.PreOperation = Relay;
+	operation.PostOperation = NULL;
+
+	callback.Version = ObGetFilterVersion();	//注册的回调的版本，参考MSDN
+	callback.OperationRegistrationCount = 1;	//回调函数数量
+	//RtlInitUnicodeString(&callback.Altitude, L"25444");
+	//callback.RegistrationContext = NULL;
+	callback.RegistrationContext = ObjectPreCallback;
+	callback.OperationRegistration = &operation;
+
+	ChangeMmVerifyCallback(TRUE);
+	ntstatus = ObRegisterCallbacks(&callback, &g_ObCallbackHandle);
+	ChangeMmVerifyCallback(FALSE);
+
+	if (!NT_SUCCESS(ntstatus))
+	{
+		DbgPrint("[-] ObRegisterCallbacks Error:%x\n", ntstatus);
+		return ntstatus;
+	}
+	DbgPrint("[+] ObRegisterCallbacks Success\n");
+	return ntstatus;
+}
+
+
+NTSTATUS RemoveHiddenObCallback()
+{
+	NTSTATUS ntstatus = STATUS_SUCCESS;
+
+	if (g_ObCallbackHandle != NULL)
+	{
+		ObUnRegisterCallbacks(g_ObCallbackHandle);
+		g_ObCallbackHandle = NULL;
+		DbgPrint("[*] ObUnRegisterCallbacks\n");
+	}
+	return ntstatus;
+}
+
+NTSTATUS DriverUnload(PDRIVER_OBJECT pDrvObj)
+{
+	NTSTATUS ntstatus = STATUS_SUCCESS;
+	RemoveHiddenObCallback();
+	return ntstatus;
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDrvObj, PUNICODE_STRING pRegPath)
+{
+	NTSTATUS ntstatus = STATUS_SUCCESS;
+
+	pDrvObj->DriverUnload = DriverUnload;
+
+	KdPrint(("DriverEntry\n"));
+
+	//((PLDR_DATA_TABLE_ENTRY)pDrvObj->DriverSection)->Flags = 0x20u;
+	RegisterHiddenObCallback();
+
+	return ntstatus;
+}
